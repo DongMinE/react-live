36d04aa69081e1f648614e476e432e6f1544d59b
김동민 <anwjr41@naver.com>
2023-07-07 17:43:25 +0900

    타입스크립트 js로 변환, 보간법

--- 00-typescript/scripts/01-let-const.js
+++ 00-typescript/scripts/01-let-const.js
@@ -1,8 +1,8 @@
+"use strict";
 // --------------------------------------------------------------------------
+// ECMAscript 2015
 // let, const (block scope)
-
-for (var i = 0, numbers = [3, 6, 9, 12]; i < numbers.length; ++i) {
-  // ...
+for (let i = 0, numbers = [3, 6, 9, 12]; i < numbers.length; ++i) {
+    // ...
 }
-
 console.log(i);
---
+++ 00-typescript/src/02-template-literal.ts
@@ -0,0 +1,84 @@
+// --------------------------------------------------------------------------
+// template literal
+
+const koreanFoods: {
+  caption: string;
+  rows: {
+    headline: string;
+    content: string;
+  }[];
+} = {
+  caption: '한식 메뉴',
+  rows: [
+    {headline: '뚝배기 불고기', content: '8,000원'},
+    {headline: '스팸치즈볶음밥', content: '7,500원'},
+    {headline: '불고기낙지덮밥', content: '9,000원'},
+  ],
+};
+
+function run() {
+  let rendredResult = printTableHTML(koreanFoods);
+  rendredResult = removeSpaceString(rendredResult);
+  console.log(rendredResult);
+}
+function removeSpaceString(data: string): string  {
+  return data.replace(/(\s+<$>\s+)/g, data($1) => {
+    if (/\s+<$/.test($1)) {
+      return '<';
+    }else if (/>\s+/.test($1)) {
+      return '>';
+    }else {
+      return '';
+    });
+} 
+//영상 39분23초@@@@@@@@@@@@@@@@@@@@@@@@
+function printTableHTML(data: {
+  caption: string;
+  rows: {
+    headline: string;
+    content: string;
+  }[];
+}): string {
+  // let variable = 'string';
+  // return `this is ${variable}`;
+  return `
+  <table class="table">
+    <caption class="sr-only">${data.caption}</caption>
+    ${
+    data.rows.map(item => (
+        `
+        <tr>
+          <th>${item.headline}</th>
+          <td>${item.content}</td>
+        </tr>
+        `
+    )).join('')}
+   </table>
+  `;
+
+}
+
+function renderTable(data: {
+  caption: string;
+  rows: {
+    headline: string;
+    content: string;
+  }[];
+}): string {
+  return [
+    '<table class="table">',
+    '<caption class="sr-only">' + data.caption + '</caption>',
+    data.rows.reduce(function (htmlString, rowData) {
+      const rowString = [
+        '<tr>',
+        '<th>' + rowData.headline + '</th>',
+        '<td>' + rowData.content + '</td>',
+        '</tr>',
+      ].join('');
+      return htmlString + rowString;
+    }, ''),
+    '</table>',
+  ].join('');
+}
+
+run();
