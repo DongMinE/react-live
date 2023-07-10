fa31d293743746dfde36e41761c59b9387e99a08
김동민 <anwjr41@naver.com>
2023-07-11 02:38:46 +0900

    2일차 07-11 how VS what

--- 00-typescript/scripts/main.js
+++ 00-typescript/scripts/main.js
@@ -19,4 +19,63 @@
 // 11. promise & async - await
 // 12. ES Modules
 // 13. class
-console.log('안녕gggg');
+// console.log("es module");
+
+import createRoot, { shuffle, createElement as h } from "./12-es-modules.js";
+
+// console.log(createRoot);
+
+function demo1() {
+  console.log(Esmodules.shuffle([1, 2, 3, 4]));
+  console.log(Esmodules.numberWithComma(9_700_234));
+
+  const originalArray = [2, 4, 65, 23];
+  const createdArray1 = [...originalArray];
+  const createdArray2 = originalArray;
+  const createdArray3 = shuffle(originalArray);
+
+  console.assert(
+    Object.is(originalArray, createdArray1),
+    "createdArray1 동일한 객체아니다"
+  );
+  console.assert(
+    !Object.is(originalArray, createdArray2),
+    "createdArray2 동일한 객체이다"
+  );
+  console.assert(
+    Object.is(originalArray, createdArray3),
+    "createdArray3 동일한 객체아니다"
+  );
+}
+
+function demo2() {
+  //가상 노드
+  const stringElement = h("strong", {}, "Virtual");
+  const headingElement = h(
+    "h1",
+    { className: "headline", lang: "en" },
+    stringElement,
+    " Element"
+  );
+
+  const appElement = h(
+    "div",
+    {
+      className: "app",
+      role: "section",
+    },
+    headingElement
+  );
+
+  console.log(appElement); //vDom tree
+
+  const rootElement = document.getElementById("root");
+  const virtualDomRoot = createRoot(rootElement);
+  // console.log(virtualDomRoot);
+
+  virtualDomRoot.render(appElement);
+  //가상노드를 render메소드를 통해 생성된 인스턴스에 넣는다.
+  // virtualDomRoot.render(headingElement);
+}
+
+demo2();
--- 01-functional-programming/2-declarative.js
+++ 01-functional-programming/2-declarative.js
@@ -5,26 +5,30 @@
 // --------------------------------------------------------------------------
 
 const originalValue = [
-  { name: '명령형 프로그래밍' },
-  { name: '선언형 프로그래밍' },
+  { name: "명령형 프로그래밍" },
+  { name: "선언형 프로그래밍" },
 ];
 
 const expectedValue = [
   {
-    key: 'programming-1',
-    name: '명령형 프로그래밍',
+    key: "programming-1",
+    name: "명령형 프로그래밍",
   },
   {
-    key: 'programming-2',
-    name: '선언형 프로그래밍',
+    key: "programming-2",
+    name: "선언형 프로그래밍",
   },
 ];
 
 function run() {
-  let receivedValue;
+  // let receivedValue = originalValue.slice();
+  // let receivedValue = [...originalValue];
 
   // 선언형 코드를 여기에 작성합니다.
-
+  let receivedValue = originalValue.map((item, index) => {
+    item.key = `programming-${index + 1}`;
+    return item;
+  });
   return receivedValue;
 }
 
@@ -35,5 +39,5 @@ const [expectedfirstItem] = expectedValue;
 
 console.assert(
   Object.is(receivedFirstItem.key, expectedfirstItem.key),
-  '⚠️ receivedFirstItem와 expectedfirstItem의 key 값이 다릅니다.'
+  "⚠️ receivedFirstItem와 expectedfirstItem의 key 값이 다릅니다."
 );
