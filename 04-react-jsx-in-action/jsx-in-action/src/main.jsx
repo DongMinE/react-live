38ba7acd32063bb41dfe89605fdb25788e32b9a2
김동민 <anwjr41@naver.com>
2023-07-16 02:25:36 +0900

    3일차 완강

--- 04-react-jsx-in-action/jsx-in-action/src/main.jsx
+++ 04-react-jsx-in-action/jsx-in-action/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/global.css';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+
+// [개발 모드 활성 : React에서 컴포넌트의 순수함을 확인하는 방법]
+// 참고: https://react.dev/reference/react/StrictMode
+// INPUT (props) -> COMPONENT -> OUTPUT (JSX) ⌝
+//                                         동일해야 함
+// INPUT (props) -> COMPONENT -> OUTPUT (JSX) ⌟
+
+createRoot(rootElement).render(
+  // StrictMode : 순수한가 순수하지 않은가??
+  // 함수 컴포넌트가 순수한지는 어떻게 체크함?
+  // 렌더링을 두번 해서 같은 아웃풋인지
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
