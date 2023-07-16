afb86915cc500f3569c08a087399291be981fa7a
김동민 <anwjr41@naver.com>
2023-07-16 22:25:28 +0900

    07-23 20:25 컴포넌트 분리중

---
+++ 05-react-component-props/src/components/index.js
@@ -0,0 +1,6 @@
+// import DescriptionList from './DescriptionList/DescriptionList';
+// export default {
+//   DescriptionList,
+// };
+
+export { default as DescriptionList } from './DescriptionList/DescriptionList';
