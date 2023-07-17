fdaa5aab35dac4af106d6dd728d26c80c25f6cd3
김동민 <anwjr41@naver.com>
2023-07-18 02:24:17 +0900

    0718 2:23 4회차-2수강중

--- 05-react-component-props/src/app/App.jsx
+++ 05-react-component-props/src/app/App.jsx
@@ -1,20 +1,19 @@
-import { DescriptionList } from '../components';
-import ScrollButton, { ScrollButtonGroup } from '../components/ScrollButton/ScrollButton';
+import { DescriptionList, ScrollButton } from '../components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>React 컴포넌트 &amp; Props</h1>
 
       <hr />
 
       <DescriptionList />
 
-      <ScrollButtonGroup>
-        <ScrollButton />
-        <ScrollButton />
-      </ScrollButtonGroup>
+      <ScrollButton.Group>
+        <ScrollButton mode='down' label='App 영역 끌어 내리기' />
+        <ScrollButton mode='up' />
+      </ScrollButton.Group>
     </div>
   );
 }
